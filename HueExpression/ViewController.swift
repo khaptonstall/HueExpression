283d4ba064c63760eab200a84b03276772818678
khaptonstall <khaptonstall@solstice-consulting.com>
2018-01-08 21:37:52 -0600

    Initial Commit

---
+++ HueExpression/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HueExpression
+//
+//  Created by Kyle Haptonstall on 1/8/18.
+//  Copyright © 2018 Kyle Haptonstall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
