02f96bf81c7cfc3d4180d65f6d70d8a2c20f4342
khaptonstall <khaptonstall@solstice-consulting.com>
2018-02-18 22:37:37 -0600

    added enum for HTTPMethods

---
+++ HueExpression/Models/HTTPMethod.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPMethod.swift
+//  HueExpression
+//
+//  Created by Kyle Haptonstall on 2/18/18.
+//  Copyright © 2018 Kyle Haptonstall. All rights reserved.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
